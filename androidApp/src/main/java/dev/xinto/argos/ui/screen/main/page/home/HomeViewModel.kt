18bc223b9fe22ae5446468daacd8576ee20df227
Xinto <burtkni@yandex.ru>
2023-12-01 15:36:58 +0400

    select the current week day by default

--- androidApp/src/main/java/dev/xinto/argos/ui/screen/main/page/home/HomeViewModel.kt
+++ androidApp/src/main/java/dev/xinto/argos/ui/screen/main/page/home/HomeViewModel.kt
@@ -6,22 +6,26 @@ import dev.xinto.argos.domain.DomainResponse
 import dev.xinto.argos.domain.lectures.LecturesRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.flow.update
+import java.time.LocalDate
 
 class HomeViewModel(
     private val lecturesRepository: LecturesRepository
 ) : ViewModel() {
 
-    private val _selectedDay = MutableStateFlow(0)
+    private val _selectedDay = MutableStateFlow(LocalDate.now().dayOfWeek.value - 1)
 
     val state = combine(lecturesRepository.observeLectures(), _selectedDay) { lectures, selectedDay ->
         when (lectures) {
             is DomainResponse.Loading -> HomeState.Loading
-            is DomainResponse.Success -> HomeState.Success(selectedDay, lectures.value)
+            is DomainResponse.Success -> {
+                val selectedIndex = if (selectedDay > lectures.value.size) 0 else selectedDay
+                HomeState.Success(
+                    selectedDay = selectedIndex,
+                    lectures = lectures.value
+                )
+            }
             is DomainResponse.Error -> HomeState.Error
         }
     }.stateIn(
