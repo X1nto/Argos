a4dce4c46eaac2196490e708b24ee992a9195c5a
Xinto <burtkni@yandex.ru>
2023-12-23 22:07:02 +0400

    enable dynamic color

--- androidApp/src/main/java/dev/xinto/argos/ui/theme/ArgosTheme.kt
+++ androidApp/src/main/java/dev/xinto/argos/ui/theme/ArgosTheme.kt
@@ -1,39 +1,36 @@
 package dev.xinto.argos.ui.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Shapes
 import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun ArgosTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        darkColorScheme(
-            primary = Color(0xFFBB86FC),
-            secondary = Color(0xFF03DAC5),
-            tertiary = Color(0xFF3700B3)
-        )
-    } else {
-        lightColorScheme(
-            primary = Color(0xFF6200EE),
-            secondary = Color(0xFF03DAC5),
-            tertiary = Color(0xFF3700B3)
-        )
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+        darkTheme -> darkColorScheme()
+        else -> lightColorScheme()
     }
-    val typography = Typography()
-    val shapes = Shapes()
 
     MaterialTheme(
-        colorScheme = colors,
-        typography = typography,
-        shapes = shapes,
+        colorScheme = colorScheme,
+        typography = Typography(),
+        shapes = Shapes(),
         content = content
     )
 }
