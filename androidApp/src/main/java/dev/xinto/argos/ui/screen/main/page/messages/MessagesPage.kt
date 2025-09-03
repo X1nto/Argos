e032e7d019b5a273e40bc180ae169aac7c7c08f6
Xinto <burtkni@yandex.ru>
2025-09-03 21:10:31 +0400

    Update messages list UI

--- androidApp/src/main/java/dev/xinto/argos/ui/screen/main/page/messages/MessagesPage.kt
+++ androidApp/src/main/java/dev/xinto/argos/ui/screen/main/page/messages/MessagesPage.kt
@@ -1,14 +1,25 @@
 package dev.xinto.argos.ui.screen.main.page.messages
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ButtonGroup
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.ListItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.RadioButton
@@ -21,7 +32,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
@@ -30,8 +48,9 @@ import androidx.paging.compose.itemKey
 import dev.xinto.argos.R
 import dev.xinto.argos.domain.messages.DomainMessage
 import dev.xinto.argos.domain.messages.DomainMessageSource
-import dev.xinto.argos.ui.component.SecondaryTabPager
+import dev.xinto.argos.domain.semester.DomainSemester
 import org.koin.androidx.compose.getViewModel
+import kotlin.math.absoluteValue
 
 @Composable
 fun MessagesPage(
@@ -179,49 +198,6 @@ private fun MessagesList(
             }
 
             is LoadState.NotLoading -> {
-                LazyColumn(modifier = Modifier.fillMaxSize()) {
-                    item {
-                        if (messages.loadState.prepend is LoadState.Loading) {
-                            CircularProgressIndicator()
-                        }
-                    }
-                    items(
-                        count = messages.itemCount,
-                        key = messages.itemKey { it.id }
-                    ) {
-                        val message = messages[it]!!
-                        if (it != 0) {
-                            HorizontalDivider()
-                        }
-                        ListItem(
-                            modifier = Modifier
-                                .clickable {
-                                    onMessageClick(message.id, message.semId)
-                                },
-                            overlineContent = {
-                                Text(message.sentAt.relativeDateTime)
-                            },
-                            headlineContent = {
-                                Text(message.subject)
-                            },
-                            supportingContent = {
-                                when (val source = message.source) {
-                                    is DomainMessageSource.Inbox -> {
-                                        Text(source.sender.fullName)
-                                    }
-                                    is DomainMessageSource.Outbox -> {
-                                        Text(source.receiver.fullName)
-                                    }
-                                    is DomainMessageSource.General -> {}
-                                }
-                            }
-                        )
-                    }
-                    item {
-                        when (val appendState = messages.loadState.append) {
-                            is LoadState.Loading -> {
-                                CircularProgressIndicator()
-                            }
                 if (messages.itemCount == 0) {
                     Column(
                         horizontalAlignment = Alignment.CenterHorizontally,
@@ -252,6 +228,9 @@ private fun MessagesList(
                             if (it != 0) {
                                 HorizontalDivider()
                             }
+                            Message(
+                                modifier = Modifier.clickable { onMessageClick(message.id, message.semId) },
+                                message = message
                             )
                         }
                         item {
@@ -260,19 +239,113 @@ private fun MessagesList(
                                     CircularProgressIndicator()
                                 }
 
-                            is LoadState.Error -> {
-                                Text(appendState.error.stackTraceToString())
-                            }
+                                is LoadState.Error -> {
+                                    Text(appendState.error.stackTraceToString())
+                                }
 
-                            else -> {}
+                                else -> {}
+                            }
                         }
                     }
                 }
+
             }
 
             is LoadState.Error -> {
                 Text(state.error.stackTraceToString())
             }
         }
+    }
+}
+
+@Composable
+fun Message(
+    modifier: Modifier = Modifier,
+    message: DomainMessage
+) {
+    val user = remember(message.source) {
+        when (val source = message.source) {
+            is DomainMessageSource.General -> source.sender.fullName
+            is DomainMessageSource.Inbox -> source.sender.fullName
+            is DomainMessageSource.Outbox -> source.receiver.fullName
+        }
+    }
+    val cleanBody = remember(message.body) {
+        message.body
+            .replace("<br />", "")
+            .replace('\n', ' ')
+    }
+    Row(
+        modifier = modifier.padding(12.dp),
+        horizontalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        PrettyPlaceholderImage(
+            modifier = Modifier
+                .size(36.dp)
+                .align(Alignment.Top),
+            fullName = user
+        )
+        Column {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(user)
+                Text(
+                    text = message.sentAt.relativeDateTime,
+                    style = MaterialTheme.typography.labelMedium
+                )
+            }
+            Text(
+                text = message.subject,
+                fontSize = 14.sp,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                text = cleanBody,
+                fontSize = 14.sp,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+    }
+}
+
+@Composable
+fun PrettyPlaceholderImage(
+    modifier: Modifier = Modifier,
+    fullName: String
+) {
+    val avatarColors = remember {
+        listOf(
+            0xFFE57373.toInt(), // Red
+            0xFFF06292.toInt(), // Pink
+            0xFFBA68C8.toInt(), // Purple
+            0xFF64B5F6.toInt(), // Blue
+            0xFF4DB6AC.toInt(), // Teal
+            0xFF81C784.toInt(), // Green
+            0xFFFFB74D.toInt(), // Orange
+            0xFFA1887F.toInt(), // Brown
+            0xFF90A4AE.toInt()  // Blue Grey
+        )
+    }
+    val avatarName = fullName[0].uppercase()
+    val avatarColor = remember(fullName) {
+        val hash = fullName.hashCode().absoluteValue
+        val colorHex = avatarColors[hash % avatarColors.size]
+        Color(colorHex)
+    }
+    Box(
+       modifier = modifier.background(color = avatarColor, shape = CircleShape),
+       contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = avatarName,
+            color = MaterialTheme.colorScheme.surface,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Medium
+        )
     }
 }
