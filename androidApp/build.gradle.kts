7dd297b90e583df1adc7c60bc952fec7dc5625b4
Xinto <burtkni@yandex.ru>
2025-09-11 14:42:29 +0400

    Use different package name for debug builds

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -38,6 +38,10 @@ android {
                 "proguard-rules.pro"
             )
         }
+        getByName("debug") {
+            applicationIdSuffix = ".dev"
+            versionNameSuffix = " (dev)"
+        }
     }
 
     signingConfigs {
