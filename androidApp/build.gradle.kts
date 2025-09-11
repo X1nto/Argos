57853ff17a45bb9f1e9fe7f1e96a5ab85955ee81
Xinto <burtkni@yandex.ru>
2025-09-11 14:17:39 +0400

    Target SDK 35

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     defaultConfig {
         applicationId = "dev.xinto.argos"
         minSdk = 23
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
